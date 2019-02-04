8bd95f92170a36d13398d8a49969346a9c7c32a4
Giuseppe Naponiello <beppenapo@arc-teamcom>
2019-02-04 15:17:54 +0100

    nome

--- nome.php
+++ nome.php
@@ -3,57 +3,66 @@
   <head>
     <?php require('inc/meta.php'); ?>
     <?php require('inc/css.php'); ?>
-    <style media="screen">
-      .mainContent{
-        background-image: url('img/header01.jpg');
-        background-repeat: no-repeat;
-        background-position: top center;
-        background-size: contain;
-      }
-    </style>
   </head>
   <body>
     <div class="mainHeader bg-white fixed-top">
       <?php require('inc/header.php'); ?>
     </div>
-    <div class="mainContent">
-      <div class="container mb-5">
-        <div class="manifest rounded">
+    <div class="bannerWrap position-fixed bg-white"></div>
+    <div class="mainContent home bg-white">
+      <div class="manifest shadow">
+        <div class="container">
           <div class="row">
-            <div class="col-md-5 imgLampi p-5">
-              <img src="img/401px-Giovanni_Battista_Lampi_autoritratto_Innsbruck.jpg" alt="autoritratto" class="img-fluid rounded">
-              <small>
-                <a rel="nofollow" class="external free mr-3" href="http://www.tirolerportraits.it/de/Portraits-suchen.aspx?ctl00_ContentPlaceHolderHaupt_RadGrid1ChangePage=39" title="fonte primaria dell'immagine [link esterno]">Tirolerportraits</a>
-                <a href="https://commons.wikimedia.org/w/index.php?curid=51392264" title="licenza di utilizzo [link esterno]">pubblico dominio</a>
-              </small>
-            </div>
-            <div class="col-md-7 py-5">
-              <h4>Il nome</h4>
-              <p>Il nome dell'Associazione deriva da quello del famoso pittore <a href="https://it.wikipedia.org/wiki/Giovanni_Battista_Lampi" class="" title="Pagina di Wikipedia dedicata all'artista [link esterno]" target="_blank">Giovanni Battista Lampi</a>, nato a Romeno il 31 dicembre 1751 e morto a Vienna l'11 febbraio 1830.<br>Di seguito la scheda biografica di Roberto Pancheri, socio dell'Associazione e massimo esperto della figura del Lampi.</p>
-              <hr>
-              <p>In Val di Non si possono ammirare le opere giovanili del Lampi a:</p>
-              <ul>
-                <li>
-                  <a href="https://goo.gl/maps/Bn19MmFV9W22" target="_blank" title="Vedi su Google Maps [link esterno]"><i class="fas fa-map-marker-alt"></i></a>
-                  Sanzeno, Basilica dei Santi Martiri: Pala dei Martiri Anauniesi (1775)
-                </li>
-                <li>
-                  <a href="https://goo.gl/maps/mcGoiMvke7C2" target="_blank" title="Vedi su Google Maps [link esterno]"><i class="fas fa-map-marker-alt"></i></a>
-                  Cavareno, Chiesa di Santa Maria Maddalena: Maddalena Penitente (1776)
-                </li>
-                <li>
-                  <a href="https://goo.gl/maps/EU5xfsKQToE2" target="_blank" title="Vedi su Google Maps [link esterno]"><i class="fas fa-map-marker-alt"></i></a>
-                  Cles, convento francescano: Cristo morto (1779)
-                </li>
-                <li>
-                  <a href="https://goo.gl/maps/FAo7nGKvJoL2" target="_blank" title="Vedi su Google Maps [link esterno]"><i class="fas fa-map-marker-alt"></i></a>
-                  Romeno, affreschi nella volta del presbiterio della Chiesa di S. Maria Assunta (1773 c.a.)
-                </li>
-              </ul>
-            </p>
+            <div class="col-md-4 manifestLogo"></div>
+            <div class="col-md-8 py-3">
+              <p>L'<strong>Associazione Culturale "G. B. Lampi" - Alta Anaunia</strong> promuove lo studio e la divulgazione della cultura locale nel territorio dell'Alta Val di Non e della c.d. "Terza Sponda" in Trentino.<br>Fondata nel 1992, da vent'anni si dedica all'organizzazione di conferenze, serate culturali, convegni e alla pubblicazioni di volumi inerenti la storia, l'archeologia, la storia dell'arte e, più in generale, le manifestazioni culturali della gente d'Anaunia.</p>
+              <p class="h4 border-bottom border-dark">DIVENTA SOCIO DELLA LAMPI!</p>
+              <p>Iscriversi all'Associazione è semplice!<br>Scopri i vantaggi e le modalità d'iscrizione</p>
+              <a href="iscrizione.php" class="btn btn-primary">diventa socio</a>
             </div>
           </div>
         </div>
+      </div>
+
+    <!-- <div class="manifest rounded">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-5 imgLampi p-5">
+            <img src="img/401px-Giovanni_Battista_Lampi_autoritratto_Innsbruck.jpg" alt="autoritratto" class="img-fluid rounded">
+            <small>
+              <a rel="nofollow" class="external free mr-3" href="http://www.tirolerportraits.it/de/Portraits-suchen.aspx?ctl00_ContentPlaceHolderHaupt_RadGrid1ChangePage=39" title="fonte primaria dell'immagine [link esterno]">Tirolerportraits</a>
+              <a href="https://commons.wikimedia.org/w/index.php?curid=51392264" title="licenza di utilizzo [link esterno]">pubblico dominio</a>
+            </small>
+          </div>
+          <div class="col-md-7 py-5">
+            <h4>Il nome</h4>
+            <p>Il nome dell'Associazione deriva da quello del famoso pittore <a href="https://it.wikipedia.org/wiki/Giovanni_Battista_Lampi" class="" title="Pagina di Wikipedia dedicata all'artista [link esterno]" target="_blank">Giovanni Battista Lampi</a>, nato a Romeno il 31 dicembre 1751 e morto a Vienna l'11 febbraio 1830.<br>Di seguito la scheda biografica di Roberto Pancheri, socio dell'Associazione e massimo esperto della figura del Lampi.</p>
+            <hr>
+            <p>In Val di Non si possono ammirare le opere giovanili del Lampi a:</p>
+            <ul>
+              <li>
+                <a href="https://goo.gl/maps/Bn19MmFV9W22" target="_blank" title="Vedi su Google Maps [link esterno]"><i class="fas fa-map-marker-alt"></i></a>
+                Sanzeno, Basilica dei Santi Martiri: Pala dei Martiri Anauniesi (1775)
+              </li>
+              <li>
+                <a href="https://goo.gl/maps/mcGoiMvke7C2" target="_blank" title="Vedi su Google Maps [link esterno]"><i class="fas fa-map-marker-alt"></i></a>
+                Cavareno, Chiesa di Santa Maria Maddalena: Maddalena Penitente (1776)
+              </li>
+              <li>
+                <a href="https://goo.gl/maps/EU5xfsKQToE2" target="_blank" title="Vedi su Google Maps [link esterno]"><i class="fas fa-map-marker-alt"></i></a>
+                Cles, convento francescano: Cristo morto (1779)
+              </li>
+              <li>
+                <a href="https://goo.gl/maps/FAo7nGKvJoL2" target="_blank" title="Vedi su Google Maps [link esterno]"><i class="fas fa-map-marker-alt"></i></a>
+                Romeno, affreschi nella volta del presbiterio della Chiesa di S. Maria Assunta (1773 c.a.)
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="mainContent">
+      <div class="container mb-5">
       </div>
       <div class="container bg-white border rounded">
         <div class="row">
@@ -62,7 +71,7 @@
           </div>
         </div>
       </div>
-    </div>
+    </div> -->
     <?php require('inc/footer.php'); ?>
     <?php require('inc/lib.php'); ?>
   </body>
