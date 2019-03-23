35f772677f60a6c12c9d682f8b3fec4cc6ce48fa
Giuseppe Naponiello <beppenapo@arc-team.com>
2019-03-23 14:44:25 +0100

    post list

--- post.php
+++ post.php
@@ -4,7 +4,7 @@ require('class/eventi.class.php');
 $post = new Eventi;
 $bozza = !isset($_SESSION['id']) ? 'f' : null;
 $lista = $post->postList(null,$bozza, null);
-$postRow = array_chunk($lista, 3);
+// $postRow = array_chunk($lista, 3);
 ?>
 
 <!doctype html>
@@ -36,28 +36,32 @@ $postRow = array_chunk($lista, 3);
               <input type="search" class="form-control" name="filtraPost" placeholder="filtra post" />
               <div class="input-group-append">
                 <button type="button" name="filterReset" class="btn btn-secondary"><i class='fas fa-times'></i></button>
-                <span class="input-group-text" id="groupStat">xx/yy</span>
+                <span class="input-group-text" id="filterStat"><span><?php echo count($lista) ?></span> / <span><?php echo count($lista) ?></span></span>
               </div>
             </div>
           </div>
         </div>
-        <?php
-        foreach ($postRow as $value) {
-          echo "<div class='row post-row'>";
-          foreach ($value as $p) {
-            echo "<div class='col-md-4'>";
-              echo "<article class='card rounded-0 animation'>";
-                echo "<figure class='card-title post-banner lozad mb-0' data-background-image='upload/copertine/".$p['copertina']."'></figure>";
-                echo '<section class="card-body">';
-                  echo "<p class='post-title'>".$p['titolo']."</p>";
-                  echo "<div class='post-body text-muted'>".$post->truncate(strip_tags($p['testo'],'<br><br/><strong><b><ol><ul><li>'), 300, array('ending' => ' [...]', 'exact' => false, 'html' => true))."</div>";
-                echo '</section>';
+        <div class='row post-row'>
+          <div class="card-columns">
+            <?php
+            foreach ($lista as $p) {
+              $tag = [];
+              $tags = str_replace(array("{","}",'"'),'',$p['tag']);
+              $tags = explode(",",$tags);
+              foreach ($tags as $v) {array_push($tag,"<small class='bg-info rounded text-white p-1 mr-1 mb-1 filtro' data-filter='".$v."'>".$v."</small>");}
+              echo "<article class='card rounded-0 animation postDiv'>";
+              echo "<figure class='card-title post-banner mb-0' style='background-image:url(upload/copertine/".$p['copertina'].")'></figure>";
+              echo "<section class='card-body'>";
+              echo "<p class='post-title filtro' data-filter='".$p['titolo']."'>".$p['titolo']."</p>";
+              echo "<div class='post-body text-muted filtro' data-filter='".strip_tags($p['testo'])."'>".$post->truncate(strip_tags($p['testo'],'<br><br/><strong><b><ol><ul><li>'), 300, array('ending' => ' [...]', 'exact' => false, 'html' => true));
+              echo "<div class='d-block my-2'>".join('',$tag)."</div>";
+              echo "</div>";
+              echo "</section>";
               echo "</article>";
-            echo "</div>";
-          }
-          echo "</div>";
-        }
-        ?>
+            }
+            ?>
+          </div>
+        </div>
       </div>
       <?php require('inc/footer.php'); ?>
     </div>
@@ -70,8 +74,21 @@ $postRow = array_chunk($lista, 3);
     <?php require('inc/lib.php'); ?>
     <script type="text/javascript">
       $(".mainContent").css({"top" : $(".mainHeader").height() + 3})
-      $("[name=filtraPost]").on('change',function(){
-        console.log($(this).val());
+      $('.postDiv').hover(function(){ $(this).toggleClass("shadow"); });
+      if (!RegExp.escape) { RegExp.escape = function (value) { return value.replace(/[\-\[\]{}()*+?.,\\\^$|#\s]/g, "\\$&") }; }
+      var $div = $('.postDiv')
+      var $filtro = $div.find('.filtro');
+      $('[name=filtraPost]').keyup(function () {
+        var filter = this.value, regex;
+        if (filter.length > 2) {
+          regex = new RegExp(RegExp.escape(this.value), 'i')
+          var $found = $filtro.filter(function () { return regex.test($(this).data('filter')) }).closest('.postDiv').show();
+          $div.not($found).hide()
+        } else {
+          $div.show();
+        }
+        $("#filterStat").find('span').eq(0).text($(".postDiv:visible").length)
+        $("[name=filterReset]").on('click', function(){$("[name=filtraPost]").val('').trigger('keyup');})
       });
     </script>
   </body>
