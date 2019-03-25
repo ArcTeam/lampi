be4ecda2b6dfd20293661cf049336d7235082266
Giuseppe Naponiello <beppenapo@arc-team.com>
2019-03-25 12:42:08 +0100

    merge

--- post.php
+++ post.php
@@ -75,21 +75,21 @@ $lista = $post->postList(null,$bozza, null);
     <script type="text/javascript">
       $(".mainContent").css({"top" : $(".mainHeader").height() + 3})
       $('.postDiv').hover(function(){ $(this).toggleClass("shadow"); });
-      if (!RegExp.escape) { RegExp.escape = function (value) { return value.replace(/[\-\[\]{}()*+?.,\\\^$|#\s]/g, "\\$&") }; }
-      var $div = $('.postDiv')
-      var $filtro = $div.find('.filtro');
-      $('[name=filtraPost]').keyup(function () {
-        var filter = this.value, regex;
-        if (filter.length > 2) {
-          regex = new RegExp(RegExp.escape(this.value), 'i')
-          var $found = $filtro.filter(function () { return regex.test($(this).data('filter')) }).closest('.postDiv').show();
-          $div.not($found).hide()
-        } else {
-          $div.show();
-        }
-        $("#filterStat").find('span').eq(0).text($(".postDiv:visible").length)
-        $("[name=filterReset]").on('click', function(){$("[name=filtraPost]").val('').trigger('keyup');})
-      });
+      // if (!RegExp.escape) { RegExp.escape = function (value) { return value.replace(/[\-\[\]{}()*+?.,\\\^$|#\s]/g, "\\$&") }; }
+      // var $div = $('.postDiv')
+      // var $filtro = $div.find('.filtro');
+      // $('[name=filtraPost]').keyup(function () {
+      //   var filter = this.value, regex;
+      //   if (filter.length > 2) {
+      //     regex = new RegExp(RegExp.escape(this.value), 'i')
+      //     var $found = $filtro.filter(function () { return regex.test($(this).data('filter')) }).closest('.postDiv').show();
+      //     $div.not($found).hide()
+      //   } else {
+      //     $div.show();
+      //   }
+      //   $("#filterStat").find('span').eq(0).text($(".postDiv:visible").length)
+      //   $("[name=filterReset]").on('click', function(){$("[name=filtraPost]").val('').trigger('keyup');})
+      // });
     </script>
   </body>
 </html>
